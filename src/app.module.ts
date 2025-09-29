c62515c4c160f93943ba144b76aeb4daa9939764
SteffenLee <tnqjae@naver.com>
2025-09-29 17:16:13 +0900

    Vendor CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from "./database/prisma.module";
+import { VendorModule } from './vendor/vendor.module';
 
 @Module({
-  imports: [ PrismaModule ],
+  imports: [ PrismaModule, VendorModule ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/vendor/vendor.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { VendorService } from './vendor.service';
+import { CreateVendorDto } from './dto/create-vendor.dto';
+import { UpdateVendorDto } from './dto/update-vendor.dto';
+
+@Controller('vendors')
+export class VendorController {
+  constructor(private readonly vendorService: VendorService) {}
+
+  @Post()
+  async create(@Body() dto: CreateVendorDto) {
+    return this.vendorService.create(dto);
+  }
+
+  @Get()
+  async findAll() {
+    return this.vendorService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.vendorService.findOne(+id);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() dto: UpdateVendorDto) {
+    return this.vendorService.update(+id, dto);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string) {
+    return this.vendorService.remove(+id);
+  }
+}

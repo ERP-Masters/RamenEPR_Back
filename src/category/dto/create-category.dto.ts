ea8c4ba9e6121f0e26c067a41db5f25dfb376c62
SteffenLee <tnqjae@naver.com>
2025-09-29 20:44:25 +0900

    Create category DTO

---
+++ src/category/dto/create-category.dto.ts
@@ -0,0 +1,14 @@
+import { IsNotEmpty, IsString, IsEnum, IsEmail, IsEmpty } from "class-validator";
+import { Unit } from "@prisma/client";
+import { Injectable } from "@nestjs/common";
+
+@Injectable
+export class CreateCategoryDto {
+    @IsString()
+    @IsEmpty()
+    group: Unit;
+
+    @IsString()
+    @IsEmpty()
+    category_name: string; 
+}

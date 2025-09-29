5031478d1ed09c854bcd5b86fb7da3b2570ed392
SteffenLee <tnqjae@naver.com>
2025-09-29 17:28:13 +0900

    Update create-item.dto.ts

--- src/item/dto/create-item.dto.ts
+++ src/item/dto/create-item.dto.ts
@@ -1,25 +1,32 @@
-import { IsString, IsInt, IsEnum, IsDateString } from 'class-validator';
+import { IsString, IsInt, IsEnum, IsDateString, IsNotEmpty } from 'class-validator';
 import { Unit } from '@prisma/client';
 
 export class CreateItemDto { 
     @IsString()
+    @IsNotEmpty()
     item_id: string;
 
     @IsString()
+    @IsNotEmpty()
     category_id: number;
         
     @IsString()
+    @IsNotEmpty()
     name: string;
 
     @IsEnum(Unit)
+    @IsNotEmpty()
     unit: Unit;
 
     @IsInt()
+    @IsNotEmpty()
     unit_price: number;
 
     @IsDateString()
+    @IsNotEmpty()
     expiry_date: string;
 
     @IsInt()
+    @IsNotEmpty()
     vender_id: number;
 }

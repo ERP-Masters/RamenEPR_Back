7467859b5c45ebc05a6d04df61f583f60704ca51
SteffenLee <tnqjae@naver.com>
2025-10-01 01:31:14 +0900

    Init

---
+++ src/item/item.service.ts
@@ -0,0 +1,9 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { CreateItemDto } from "./dto/create-item.dto";
+import { UpdateItemDto } from "./dto/update-item.dto";
+
+
+@Injectable()
+export class ItemService {
+    
+}

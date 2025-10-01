bc43b8c1ceddc41aa6acac22a0f061d61ea30a0b
SteffenLee <tnqjae@naver.com>
2025-10-01 11:41:25 +0900

    refactor: delete id and change Unit type

--- src/item/entities/item.entity.ts
+++ src/item/entities/item.entity.ts
@@ -2,11 +2,10 @@ import { Unit } from '@prisma/client';
 
 export class ItemEntity {
     constructor (
-        public readonly id: number,
         public readonly item_id: string,
         public readonly category_id: number,
         public readonly name: string,
-        public readonly unit: Unit,
+        public readonly unit_id: number,
         public readonly vendor_id: number,
         public readonly unit_price: number,
         public readonly expiry_date: Date,

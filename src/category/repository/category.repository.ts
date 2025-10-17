25ffec3c583e94748b718008ff34773b70573e0f
SteffenLee <tnqjae@naver.com>
2025-10-18 03:29:52 +0900

    feat(category): fix code and add some function
    - findNotUsedCategory(): how not used categoty
    
    - changeUseState(): add change use state function

--- src/category/repository/category.repository.ts
+++ src/category/repository/category.repository.ts
@@ -1,8 +1,10 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { PrismaService } from "../../database/prisma.service";
 import { CategoryEntity } from "../entities/category.entity";
 import { CreateCategoryDto } from "../dto/create-category.dto";
 import { UpdateCategoryDto } from "../dto/update-category.dto";
+import { Branch, UseState } from "@prisma/client";
+import { truncate } from "fs";
 
 @Injectable()
 export class CategoryRepository {
@@ -15,7 +17,8 @@ export class CategoryRepository {
         return new CategoryEntity(
             category.category_id, 
             category.group , 
-            category.category_name
+            category.category_name,
+            category.isused,
         );
     }
 
@@ -27,11 +30,38 @@ export class CategoryRepository {
                 new CategoryEntity(
                     c.category_id,
                     c.group,
-                    c.category_name
+                    c.category_name,
+                    c.isused,
                 )
         );
     }
 
+    async findNotUsedCategory(): Promise<CategoryEntity[]> {
+        const category = await this.prisma.category.findMany({
+            where: { isused: UseState.NOTUSED },
+            select: {
+                category_id: true,
+                group: true,
+                category_name: true,
+                isused: true
+            }
+        })
+
+        if (!category.length) {
+            throw new NotFoundException('현재 사용되지 않는 카테고리가 없습니다. ');
+        }
+
+        return category.map(
+            (c) =>
+                new CategoryEntity(
+                    c.category_id,
+                    c.group,
+                    c.category_name,
+                    c.isused
+                ),
+        );
+
+    }
     //카레고리 수정
     async update(id: number, data: UpdateCategoryDto): Promise<CategoryEntity> {
         const category = await this.prisma.category.update({ 
@@ -44,16 +74,23 @@ export class CategoryRepository {
         return new CategoryEntity(
             category.category_id, 
             category.group , 
-            category.category_name
+            category.category_name,
+            category.isused,
         );
     }
 
-    async remove(id: number): Promise<void> {
-        await this.prisma.category.delete({ 
-            where: {
-                 category_id: id 
-                } 
+    async changeUseState(id: number, state: UseState): Promise<CategoryEntity> {
+            const category =  await this.prisma.category.update({
+                where: { category_id: id },
+                data: { isused: state },
         });
+        
+        return new CategoryEntity(
+            category.category_id, 
+            category.group , 
+            category.category_name,
+            category.isused,
+        )
     }
 
 }

d1216343764c18be098f6694017824d6b556590e
SteffenLee <tnqjae@naver.com>
2025-10-18 07:38:05 +0900

    feat(vendor): add isused dto and entity

--- src/vendor/entities/vendor.entity.ts
+++ src/vendor/entities/vendor.entity.ts
@@ -1,9 +1,12 @@
+import { UseState } from "@prisma/client";
+
 export class VendorEntity {
     constructor(
         public readonly vendor_id: number,
         public readonly name: string,
         public readonly manager: string,
         public readonly contact: string,
+        public readonly isused: UseState,
         public readonly address: string,
     ) {}
 }
